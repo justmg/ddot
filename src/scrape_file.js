abdc043152e7cc2e627f7ce816bf5ef92c025991
Muhammad Hassan Saeed <Hassan.saeed@linkedunion.com>
2024-04-22 21:46:31 +0500

    fixes

--- src/scrape_file.js
+++ src/scrape_file.js
@@ -15,16 +15,23 @@ async function main(noticeNumber, taxPayerID) {
   await page.fill("//input[contains(@name, 'Dc-a')]", taxPayerID);
   await page.click("//button[contains(@data-name, 'Dc-c')]");
 
-  await page.waitForTimeout(2000);
-
+  await page.waitForTimeout(1000);
   const nonCompliance = await page.textContent('#caption2_Dc-k');
+  await page.waitForTimeout(1000);
   const certificateNotFound = await page.textContent('#caption2_Dc-g');
+  await page.waitForTimeout(1000);
   const issueDate = await page.textContent('#caption2_Dc-h');
+  await page.waitForTimeout(1000);
   const date = await page.getAttribute('#Dc-h', 'value');
+  await page.waitForTimeout(1000);
   const issuedTo = await page.textContent('#caption2_Dc-i');
+  await page.waitForTimeout(1000);
   const issuedToName = await page.getAttribute('#Dc-i', 'value');
+  await page.waitForTimeout(1000);
   const caption2 = await page.textContent('#caption2_Dc-j');
+  await page.waitForTimeout(1000);
   const caption2Element = await page.locator('//span[@id="caption2_Dc-l" and contains(@class, "CTE") and contains(@class, "CaptionLinkText") and text()="Click here to request a current Certificate of Clean Hands for this taxpayer."]');
+  await page.waitForTimeout(1000);
   const caption2Visible = await caption2Element.isVisible();
   if (!date) {
     console.log(certificateNotFound);
